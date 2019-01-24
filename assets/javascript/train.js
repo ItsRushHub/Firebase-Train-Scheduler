2d1a7def4715009632ef104b1e60a8e50e289db2
Oran Rush <oranrush@Orans-MBP.lan1>
2019-01-24 16:12:26 -0600

    Upload6

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function(e){
     // Initialize Firebase
     var config = {
         apiKey: "AIzaSyCtVrwzWDQ0ggP9TNdeicRAezpJ3LZfoWc",
@@ -13,23 +13,28 @@ $(document).ready(function(){
   
     var database = firebase.database();
 
-    console.log(database);
 
 // Make the submit button work for the submitted form info.
     $("#submit").on("click", function(e){
         e.preventDefault();
 
-        var trainName = $("#trainName").val();
-        var destination = $("#destination").val();
-        var trainTime = $("#trainTime").val();
+        var trainName = $("#trainName").val().trim;
+        var destination = $("#destination").val().trim;
+        var trainTime = moment($("#trainTime").val().trim(), "HH:mm").subtract(10, "years").format("X");
         var frequency = $("#frequency").val();
+
+        console.log(trainName);
+        console.log(destination);
+        console.log(trainTime);
+        console.log(frequency);
+        
 
         var newTrainSchedule = {
             train: trainName,
             destination: destination,
-            nextArrival: trainTime,
-            minutesAway: frequency
-        }
+            firstTrain: trainTime,
+            frequency: frequency
+        };
 
         database.ref().push(newTrainSchedule);
 
@@ -39,31 +44,40 @@ $(document).ready(function(){
         $("#frequency").val("");
     });
 
-        database.ref().on("child_added", function(childsnapshot){
+        database.ref().on("child_added", function (childsnapshot,) {
+            
             var trainName = childsnapshot.val().train;
             var destination = childsnapshot.val().destination;
-            var trainTime = childsnapshot.val().nextArrival;
-            var frequency = childsnapshot.val().minutesAway;
+            var trainTime = childsnapshot.val().firstTrain;
+            var frequency = childsnapshot.val().frequency;
+
+                console.log('trainName from firebase', trainName);
+                console.log(destination)
+                console.log(trainTime)
+                console.log(frequency)
+
+                var tRemainder = moment().diff(moment.unix(parseInt(firstTrain)), "minutes") % frequency;
+                var tMinutes = frequency - tRemainder;
+                var nextArrival = moment().add(tMinutes, "m").format("hh:mm A");
 
 
-        
-            $("tbody").append(
-                `<tr> 
-                    <td>${trainName}</td>
-                    <td>${destination}</td>
-                    <td>${trainTime}</td>
-                    <td>${frequency}</td>
-                </tr>`)
 
-                console.log(tbody);
+            var newRow = $("<tr>").append(
+                $("<td>").text(trainName),
+                $("<td>").text(destination),
+                $("<td>").text(frequency),
+                $("<td>").text(tRemainder),
+                $("<td>").text(nextArrival),
+                $("<td>").text(tMinutes),
+
+                $("<td>").append('<i class="fa fa-trash" aria-hidden="true"></i>') 
+            );
+            
+            $("#tbody").append(newRow);
+
+            // console.log(newRow);
+
+            
+
         });
-
-
-
-
-
-
-
-
-
 });
